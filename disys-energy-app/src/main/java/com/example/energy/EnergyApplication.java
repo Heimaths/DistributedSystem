9c493e39d95dcca055ac452959c66a11fc36535d
davidjamnig <djamnig@gmx.net>
2025-03-06 18:28:58 +0100

    pushthebutton

--- disys-energy-app/src/main/java/com/example/energy/EnergyApplication.java
+++ disys-energy-app/src/main/java/com/example/energy/EnergyApplication.java
@@ -2,8 +2,10 @@ package com.example.energy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling  // Diese Annotation sorgt dafür, dass @Scheduled Methoden laufen
 public class EnergyApplication {
     public static void main(String[] args) {
         SpringApplication.run(EnergyApplication.class, args);
