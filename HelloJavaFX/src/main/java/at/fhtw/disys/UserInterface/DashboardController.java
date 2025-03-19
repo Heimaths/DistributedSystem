9c5cd363a0e5bf9e98e301d69ed66181ed347705
Heimaths <120481865+Heimaths@users.noreply.github.com>
2025-03-19 19:38:22 +0100

    push

--- HelloJavaFX/src/main/java/at/fhtw/disys/UserInterface/DashboardController.java
+++ HelloJavaFX/src/main/java/at/fhtw/disys/UserInterface/DashboardController.java
@@ -1,14 +1,12 @@
 package at.fhtw.disys.UserInterface;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.DatePicker;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.VBox;
-
 import java.time.LocalDate;
+import java.time.LocalTime;
 
 public class DashboardController {
 
@@ -21,15 +19,22 @@ public class DashboardController {
     @FXML private Label communityProducedLabel;
     @FXML private Label communityUsedLabel;
     @FXML private Label gridUsedLabel;
-
+    @FXML private DatePicker startDatePicker;
+    @FXML private Spinner<Integer> startTimeSpinner;
+    @FXML private DatePicker endDatePicker;
+    @FXML private Spinner<Integer> endTimeSpinner;
     private LocalDate startDate;
     private LocalDate endDate;
 
     @FXML
     private void initialize() {
-        if (startDateButton != null && endDateButton != null) {
+        if (startDateButton != null && endDateButton != null)
+        {
             startDateButton.setOnAction(event -> selectDate(true));
             endDateButton.setOnAction(event -> selectDate(false));
+
+            startTimeSpinner.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(0, 23, LocalTime.now().getHour()));
+            endTimeSpinner.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(0, 23, LocalTime.now().getHour()));
         } else {
             System.out.println("Error: FXML elements not injected properly.");
         }
