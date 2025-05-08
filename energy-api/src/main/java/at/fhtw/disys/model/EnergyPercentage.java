e9111a3ca2385ec277b18974e1e336df87332d7d
simonjaeger1 <simon_jaeger@gmx.at>
2025-05-08 19:21:57 +0200

    added @Data to EnergyPercentage.java

--- energy-api/src/main/java/at/fhtw/disys/model/EnergyPercentage.java
+++ energy-api/src/main/java/at/fhtw/disys/model/EnergyPercentage.java
@@ -1,10 +1,12 @@
 package at.fhtw.disys.model;
 
 import jakarta.persistence.*;
+import lombok.Data;
 import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "energy_percentage")
+@Data // @Data generiert automatisch Getter & Setter und andere Methoden (z.B. toString()) -> wie in EnergyUsage.java
 public class EnergyPercentage {
 
     @Id
@@ -17,37 +19,4 @@ public class EnergyPercentage {
 
     private double gridPortion;
 
-    // Getter & Setter
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public LocalDateTime getHour() {
-        return hour;
-    }
-
-    public void setHour(LocalDateTime hour) {
-        this.hour = hour;
-    }
-
-    public double getCommunityDepleted() {
-        return communityDepleted;
-    }
-
-    public void setCommunityDepleted(double communityDepleted) {
-        this.communityDepleted = communityDepleted;
-    }
-
-    public double getGridPortion() {
-        return gridPortion;
-    }
-
-    public void setGridPortion(double gridPortion) {
-        this.gridPortion = gridPortion;
-    }
 }
